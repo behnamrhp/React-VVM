5c847213ecbd474d0b5f8758822795820f0cc4c7
behnam <behnamrahimpour74@gmail.com>
2025-03-18 11:00:15 +0300

    feature: Add base linting and ts configs

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,9 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
 import tseslint from 'typescript-eslint'
+import airbnb from 'eslint-config-airbnb'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,12 +18,30 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettier,
+      "airbnb": airbnb,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      "prettier/prettier": [
+        "warn",
+        {
+          "printWidth": 80,
+          "tabWidth": 2,
+          "endOfLine": "auto",
+          "useTabs": false,
+          "semi": true,
+          "singleQuote": false,
+          "quoteProps": "as-needed",
+          "jsxSingleQuote": false,
+          "trailingComma": "all",
+          "bracketSpacing": true,
+          "arrowParens": "always"
+        }
       ],
     },
   },
